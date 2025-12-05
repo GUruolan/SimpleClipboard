b855d85874a1eb240a6a8f63b82b66332e27ac0c
guruolan <guruolan@meituan.com>
2025-12-05 14:01:37 +0800

    Initial Commit

---
+++ SimpleClipboard/SimpleClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleClipboardApp.swift
+//  SimpleClipboard
+//
+//  Created by Navy on 2025/12/5.
+//
+
+import SwiftUI
+
+@main
+struct SimpleClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleClipboardTests/SimpleClipboardTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleClipboardTests.swift
+//  SimpleClipboardTests
+//
+//  Created by Navy on 2025/12/5.
+//
+
+import Testing
+
+struct SimpleClipboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
